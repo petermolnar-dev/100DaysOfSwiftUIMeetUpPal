24f332b4fc848c53fa7145e1253ad458736b16f6
Peter Molnar <petermolnar.hu@gmail.com>
2022-08-29 14:28:52 +0200

    Initial Commit

---
+++ MeetUpPal/MeetUpPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetUpPalApp.swift
+//  MeetUpPal
+//
+//  Created by Peter Molnar on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MeetUpPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
